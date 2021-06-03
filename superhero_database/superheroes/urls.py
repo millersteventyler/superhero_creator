732b9b0ece7fa47739d5c42b99877b9131169c5d
millersteventyler <millersteventyler@gmail.com>
2021-06-03 14:30:36 -0400

    Created detail.html and a method in views.py in order to querey for a specific superhero.

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:superhero_id>/', views.detail, name='detail')
 ]
