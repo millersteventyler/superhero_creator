85ad2b5d84fcd5c8391c20b420abfd5a8067cbd7
millersteventyler <millersteventyler@gmail.com>
2021-06-03 16:36:36 -0400

    Created ccreate.html and made it so i could post submissions from the template.

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:superhero_id>/', views.detail, name='detail')
+    path('<int:superhero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create_new_superhero')
 ]
--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Superhero
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -18,3 +19,17 @@ def detail(request, superhero_id):
         'specific_superhero': specific_superhero
     }
     return render(request, 'superheroes/detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_superhero_ability = request.POST.get('primary_superhero_ability')
+        secondary_superhero_ability = request.POST.get('secondary_superhero_ability')
+        catch_phrase = request.POST.get('catch_phrase')
+        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_superhero_ability=primary_superhero_ability, secondary_superhero_ability=secondary_superhero_ability, catch_phrase=catch_phrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
